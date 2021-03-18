87527e5d97ac4cb97b992a16d361789c510b6e07
Maggie Jablinski <mjablinski@ymail.com>
2021-03-17 22:07:23 -0400

    First commit

---
+++ Assignment09/Source.cpp
@@ -0,0 +1,18 @@
+/*
+* Maggie Jablinski
+* CIS 1202 501
+* March 17, 2021
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+
+	cout << "Hello World" << endl; 
+
+	system("pause");
+	return 0;
+}
